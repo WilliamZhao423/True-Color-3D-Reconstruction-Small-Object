469a54057d11790eb93f9d28585d3debcfb06592
Yimin Zhao <80491146+WilliamZhao423@users.noreply.github.com>
2026-04-06 18:16:16 -0400

    Create types.h

---
+++ lidar_slam_3d/include/lidar_slam_3d/types.h
@@ -0,0 +1,24 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <vector>
+#include <map>
+#include <string>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+namespace lidar_slam_3d{
+struct PosePQ{
+    Eigen::Vector3d p;
+    Eigen::Quaterniond q;
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+
+struct LoopConstraint{
+    int first_;
+    int second_;
+    Eigen::Matrix4f loop_pose_; 
+};
+
+} //namespace lidar_slam_3d
+#endif
